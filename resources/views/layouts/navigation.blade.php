fadcbd089036462ed35e60a2087429a30817aac5
Sayli Raut <sayliraut@gmail.com>
2026-01-30 22:11:01 +0530

    category

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('category')" :active="request()->routeIs('category')">
+                        {{ __('category') }}
+                    </x-nav-link>
                 </div>
             </div>
 
