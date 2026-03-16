d670ecb3e0f85ad6f77317db5eb07b90ce8dcd17
Sayli Raut <sayliraut@gmail.com>
2026-03-16 22:26:47 +0530

    check

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Ticket;
 use App\Models\Category;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
