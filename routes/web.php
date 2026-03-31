d96d38d3d4299b05c8b0c4abf3f98b779b2cb122
Sayli Raut <sayliraut@gmail.com>
2026-03-31 21:41:09 +0530

    test

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/requests/{ticket}', [TicketController::class, 'show'])->name('tickets.show');
 });
 
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/admin/requests', [TicketController::class, 'adminIndex'])->name('tickets.admin-index');
     Route::put('/admin/requests/{ticket}/status', [TicketController::class, 'updateStatus'])->name('tickets.update-status');
